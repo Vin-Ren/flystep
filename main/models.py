f4d49485952b6302e93c911b05313b3ca91cba84
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2025-09-09 00:07:23 +0700

    feat: Simple main app

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    description = models.TextField()
+    thumbnail = models.URLField(max_length=255)
+    category = models.CharField(max_length=255)
+    is_featured = models.BooleanField(default=False)
+    stock = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True) # New product spotlight
+    updated_at = models.DateTimeField(auto_now=True) # Kalo ada update, mungkin temporary featured?
--- main/views.py
+++ main/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+from .models import Product
+
 # Create your views here.
+def render_main(req):
+    context = {
+        'user': {
+            'name': 'Vincent Valentino Oei',
+            'npm': '2406353225',
+            'class': 'PBP E'
+        },
+        'products': Product.objects.all()
+    }
+    
+    return render(req, "main.html", context=context)
