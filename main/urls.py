e9ad4ca851f23c1e1f07cae35fe1df04d1a487b2
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2025-09-13 23:17:07 +0700

    feat: Finish Checkpoint for Assignment 3

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from main.views import render_main
+from main.views import render_all_json, render_all_xml, render_by_id_json, render_by_id_xml, render_create_product, render_main, render_product_details
 
 app_name = 'main'
 
 urlpatterns = [
     path('', render_main, name='show_main'),
+    path('xml/', render_all_xml, name='render_all_xml'),
+    path('xml/<str:id>/', render_by_id_xml, name='render_by_id_xml'),
+    path('json/', render_all_json, name='render_all_json'),
+    path('json/<str:id>/', render_by_id_json, name='render_by_id_json'),
+    path('create_product/', render_create_product, name='render_create_product'),
+    path('products/<str:id>/', render_product_details, name='render_product_details'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
+from django.core import serializers
+
+from .forms import ProductForm
 
 from .models import Product
 
 # Create your views here.
-def render_main(req):
+def render_main(req: HttpRequest):
     context = {
         'user': {
             'name': 'Vincent Valentino Oei',
@@ -14,3 +18,43 @@ def render_main(req):
     }
     
     return render(req, "main.html", context=context)
+
+def render_all_xml(req: HttpRequest):
+    products = Product.objects.all()
+    serialized = serializers.serialize('xml', products)
+    return HttpResponse(serialized, content_type="application/xml")
+
+def render_by_id_xml(req: HttpRequest, id):
+    product = get_object_or_404(Product, pk=id)
+    serialized = serializers.serialize('xml', [product]) # turns out it must be iterable lol, TIL.
+    return HttpResponse(serialized, content_type="application/xml")
+
+def render_all_json(req: HttpRequest):
+    products = Product.objects.all()
+    serialized = serializers.serialize('json', products)
+    return HttpResponse(serialized, content_type="application/json")
+
+def render_by_id_json(req: HttpRequest, id):
+    product = get_object_or_404(Product, pk=id)
+    serialized = serializers.serialize('json', [product]) # turns out it must be iterable lol, TIL.
+    return HttpResponse(serialized, content_type="application/json")
+
+def render_create_product(req: HttpRequest):
+    form = ProductForm(req.POST or None)
+    
+    if req.method=='POST' and form.is_valid():
+        form.save()
+        return redirect('main:show_main')
+    
+    ctx = {
+        'form': form
+    }
+    
+    return render(req, "create_product.html", context=ctx)
+
+def render_product_details(req: HttpRequest, id):
+    product = get_object_or_404(Product, pk=id)
+    ctx = {
+        'product': product
+    }
+    return render(req, 'details.html', context=ctx)
